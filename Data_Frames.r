6eb25b99bcbcc113b7c27b0672a7c6c9b4b3bd5c
Marc N <151791276+princ3Cr0w@users.noreply.github.com>
2024-02-14 21:08:43 -0500

    Update Data_Frames.r

--- Data_Frames.r
+++ Data_Frames.r
@@ -47,7 +47,9 @@ output: html_document
 ---
 
 ## Background for this activity
-This activity is focused on creating and using data frames in `R`. A data frame is a collection of columns containing data, similar to a spreadsheet or SQL table. Data frames are one of the basic tools you will use to work with data in `R`. And you can create data frames from different data sources.  
+
+#This activity is focused on creating and using data frames in `R`. A data frame is a collection of columns containing data, similar to a spreadsheet or SQL table. 
+#Data frames are one of the basic tools you will use to work with data in `R`. And you can create data frames from different data sources.  
 
 There are three common sources for data:
 
